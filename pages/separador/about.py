d63ecae7afd2973ae2590bffa2ea25628678c464
thaleson <thaleson19@hotmail.com>
2024-07-30 18:18:59 -0300

    change in about code

--- pages/separador/about.py
+++ pages/separador/about.py
@@ -13,11 +13,8 @@ def app():
     - **Python**
     - **TensorFlow**
     - **Streamlit**
-<<<<<<< HEAD:pages/separador/about.py
-    - **Rede Neurais**
-=======
     - **Redes Neurais**
->>>>>>> 9c85668dcca1d3b9a0b7e3d25d46681b601831e9:pages/about.py
+
 
     Para mais informações, entre em contato com nossa equipe.
     ''')
