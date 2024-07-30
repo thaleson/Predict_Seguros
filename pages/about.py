a50c292c79ac9e325c730fa3a1aec7e5195d3dce
felipe thaleson <thaleson19@hotmail.com>
2024-07-30 16:12:57 -0300

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
-
 def app():
     st.title('Sobre o Projeto')
     st.markdown('''
     Este projeto utiliza aprendizado de máquina para prever o valor de seguros. 
-    Nossa abordagem inclui a análise de dados históricos para fornecer estimativas precisas.
+    Nossa abordagem inclui a análise de dados históricos e o uso de redes neurais para fornecer estimativas precisas.
 
     ### Propósito
     O principal objetivo é ajudar consumidores e empresas a entenderem melhor como os valores de seguros são determinados.
@@ -14,7 +13,7 @@ def app():
     - **Python**
     - **TensorFlow**
     - **Streamlit**
-    - **Rede neurais**
+    - **Redes Neurais**
 
     Para mais informações, entre em contato com nossa equipe.
     ''')
