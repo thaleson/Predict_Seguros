cbaf6426ac75fcd90c6148066c7fd10aa257e00e
felipe thaleson <thaleson19@hotmail.com>
2024-07-30 17:02:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 
 # Função para carregar o CSS
 def load_css():
@@ -9,31 +8,20 @@ def load_css():
 # Carregar o CSS
 load_css()
 
+# Adicionando o nome do app e as rotas na sidebar
 with st.sidebar:
-  
+    st.header("Meu Projeto")
     
-    # Definindo o menu lateral com estilo
-    selected = option_menu(
-        menu_title="Menu",  
-        options=["Home", "About", "Data Visualization", "Model Prediction"],
-        icons=["house", "info-circle", "bar-chart", "calculator"],
-        menu_icon="cast",
-        default_index=0,
-        orientation="vertical",  # Alinha os itens do menu verticalmente
-     
-    )
-
-# Função para chamar a página correspondente
-if selected == "Home":
-    from pages import home
-    home.app()
-elif selected == "About":
-    from pages import about
-    about.app()
-elif selected == "Data Visualization":
-    from pages import data_visualization
-    data_visualization.app()
-elif selected == "Model Prediction":
-    from pages import model_prediction
-    model_prediction.app()
-
+    # Links para as páginas
+    if st.button("Home"):
+        from pages import home
+        home.app()
+    if st.button("About"):
+        from pages import about
+        about.app()
+    if st.button("Data Visualization"):
+        from pages import data_visualization
+        data_visualization.app()
+    if st.button("Model Prediction"):
+        from pages import model_prediction
+        model_prediction.app()
