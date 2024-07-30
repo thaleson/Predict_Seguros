5422fc457a98982ee2fcfc394f6cfc4170faa3cd
felipe thaleson <thaleson19@hotmail.com>
2024-07-30 16:28:17 -0300

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -10,3 +10,16 @@ def app():
     ''')
     st.markdown('### Comprar Seguro')
     st.image('https://modesttipittolseguros.com.br/uploads/images/183-256-orig.jpg?=v1', caption='Imagem de Seguro')
+
+    # Link para o LinkedIn
+    st.markdown('''
+    <hr>
+    <div style='text-align: center;'>
+        <p>Site desenvolvido por Thaleson Silva</p>
+        <a href='https://www.linkedin.com/in/thaleson-silva-9298a0296/' target='_blank'>
+            <button style='background-color: #0073b1; color: white; padding: 10px; border: none; border-radius: 5px;'>
+                LinkedIn
+            </button>
+        </a>
+    </div>
+    ''', unsafe_allow_html=True)
